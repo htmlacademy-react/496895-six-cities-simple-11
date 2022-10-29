1191af71b8b08b3cb134e9feaab326b9f5650cd2
TroshinSerg <serge-troshin@yandex.ru>
2022-10-29 21:19:09 +0300

    Добавляет компонент для страницы 404

---
+++ project/src/pages/not-found-screen/hot-found-screen.tsx
@@ -0,0 +1,22 @@
+import Header from '../../components/header/header';
+import './not-found.css'
+
+function NotFoundScreen(): JSX.Element {
+  return (
+    <div className="page">
+      <Header />
+      <main className="page__main page__main--property">
+        <section className="not-found">
+          <div className="not-found__container container">
+          <div className="not-found__status-wrapper">
+              <p><b className="not-found__status">404 Not Found</b></p>
+              <a className="not-found__button button" href="/">Homepage</a>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
