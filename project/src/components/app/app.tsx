2ee1142e960b51e3a7039dba981854047d371366
TroshinSerg <serge-troshin@yandex.ru>
2022-10-30 14:57:55 +0300

    Добавляет переадресацию на главную со страницы login

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,10 +4,11 @@ import RoomScreen from '../../pages/room-screen/room-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import {Offer} from '../../types/types';
 import {AppRoute} from '../../constants/constants';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 
 type AppProps = {
   offers: Offer[];
+  isAuth: boolean;
 }
 
 function App(props: AppProps): JSX.Element {
@@ -15,7 +16,7 @@ function App(props: AppProps): JSX.Element {
     <BrowserRouter>
       <Routes>
         <Route path={AppRoute.Root} element={<MainScreen offers={props.offers} />} />
-        <Route path={AppRoute.Login} element={<LoginScreen />} />
+        <Route path={AppRoute.Login} element={props.isAuth ? <Navigate to={AppRoute.Root} /> : <LoginScreen />} />
         <Route path={`${AppRoute.Room}/:id`} element={<RoomScreen />} />
         <Route path="*" element={<NotFoundScreen />} />
       </Routes>
