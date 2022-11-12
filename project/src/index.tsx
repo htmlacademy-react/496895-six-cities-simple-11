ee34e5101c6a71ceb01ae6e0dbb1e450ccfdc3f8
TroshinSerg <serge-troshin@yandex.ru>
2022-11-12 18:05:45 +0300

    Добавляет моки города

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {offers} from './mocks/offers';
 import {reviews} from './mocks/reviews';
+import {city} from './mocks/city';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,6 +12,6 @@ const isAuth = true;
 
 root.render(
   <React.StrictMode>
-    <App isAuth={isAuth} reviews={reviews} offers={offers} />
+    <App isAuth={isAuth} reviews={reviews} offers={offers} city={city} />
   </React.StrictMode>,
 );
