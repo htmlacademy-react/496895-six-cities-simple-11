72aa39cd83394ae11e5ca285460e8897cd5bbd95
TroshinSerg <serge-troshin@yandex.ru>
2022-11-12 18:00:09 +0300

    Добавляет пользовательский хук

---
+++ project/src/hooks/useMap.ts
@@ -0,0 +1,34 @@
+import {useEffect, useState, useRef, MutableRefObject} from 'react';
+import {Map, TileLayer} from 'leaflet';
+import {City} from '../types/types';
+import {LeafletLayer} from '../constants/constants';
+
+function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City): Map | null {
+  const [map, setMap] = useState<Map | null>(null);
+  const isRenderedRef = useRef(false);
+
+  useEffect(() => {
+    if (mapRef.current !== null && !isRenderedRef.current) {
+      const instance = new Map(mapRef.current, {
+        center: {
+          lat: city.location.latitude,
+          lng: city.location.longitude,
+        },
+        zoom: city.location.zoom,
+      });
+
+      const layer = new TileLayer(LeafletLayer.Link, {
+        attribution: LeafletLayer.Attribution,
+      });
+
+      instance.addLayer(layer);
+
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, city]);
+
+  return map;
+}
+
+export default useMap;
