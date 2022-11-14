01ce5ac580e9aa93df9dbc03a0b84ff15d3ec437
TroshinSerg <serge-troshin@yandex.ru>
2022-11-14 20:30:20 +0300

    Дополняет моки

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -1,4 +1,4 @@
-import {Offer} from '../types/types';
+import {TOffer} from '../types/types';
 import {city} from './city';
 
 const photos: string[] = ['img/room.jpg', 'img/apartment-01.jpg', 'img/apartment-02.jpg', 'img/apartment-03.jpg', 'img/studio-01.jpg', 'img/apartment-01.jpg'];
@@ -7,7 +7,7 @@ const goods: string[] = ['Wi-Fi', 'Washing machine', 'Towels', 'Heating', 'Coffe
 
 const description = 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.';
 
-const offers: Offer[] = [
+const offers: TOffer[] = [
   {
     bedrooms: 4,
     city: city,
@@ -140,4 +140,85 @@ const offers: Offer[] = [
   }
 ];
 
-export {offers};
+const nearbyOffers: TOffer[] = [
+  {
+    bedrooms: 4,
+    city: city,
+    description: description,
+    goods: goods,
+    host: {
+      avatarUrl: 'img/avatar-angelina.jpg',
+      id: 2,
+      name: 'Angelina',
+      isPro: true,
+    },
+    id: 6,
+    images: photos,
+    isPremium: false,
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.964309666406198,
+      zoom: 8,
+    },
+    maxAdults: 4,
+    previewImage: 'img/room.jpg',
+    price: 80,
+    rating: 3.7,
+    title: 'Wood and stone place ',
+    type: 'Private room'
+  },
+  {
+    bedrooms: 4,
+    city: city,
+    description: description,
+    goods: goods,
+    host: {
+      avatarUrl: 'img/avatar-angelina.jpg',
+      id: 2,
+      name: 'Angelina',
+      isPro: true,
+    },
+    id: 7,
+    images: photos,
+    isPremium: false,
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
+      zoom: 8,
+    },
+    maxAdults: 4,
+    previewImage: 'img/apartment-02.jpg',
+    price: 132,
+    rating: 4.1,
+    title: 'Canal View Prinsengracht',
+    type: 'Apartment'
+  },
+  {
+    bedrooms: 4,
+    city: city,
+    description: description,
+    goods: goods,
+    host: {
+      avatarUrl: 'img/avatar-angelina.jpg',
+      id: 2,
+      name: 'Angelina',
+      isPro: true,
+    },
+    id: 8,
+    images: photos,
+    isPremium: true,
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 8,
+    },
+    maxAdults: 4,
+    previewImage: 'img/apartment-03.jpg',
+    price: 180,
+    rating: 4.6,
+    title: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment'
+  },
+];
+
+export {offers, nearbyOffers};
