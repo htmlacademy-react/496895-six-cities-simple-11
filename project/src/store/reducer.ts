cd76f16ab0bbcfaea146099924ad434489dbaed7
TroshinSerg <serge-troshin@yandex.ru>
2022-11-21 15:10:04 +0300

    Добавляет кейсы сортировки в редьюсер

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,12 +1,13 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {getOffersAction, changeCityAction} from './action';
+import {getOffersAction, changeCityAction, sortByPopularAction, sortByPriceLowToHighAction, sortByPriceHighToLowAction, sortByRatedFirstAction, changeSortingTypeAction} from './action';
 import {offers} from '../mocks/offers';
-import {DEFAULT_CITY_NAME} from '../constants/constants';
+import {DEFAULT_CITY_NAME, DEFAULT_OFFERS_SORTING_OPTION} from '../constants/constants';
 
 const initialState = {
   currentCityName: DEFAULT_CITY_NAME,
   offers: offers,
   offersByCurrentCity: offers.filter((offer) => offer.city.name === DEFAULT_CITY_NAME),
+  sortingType: DEFAULT_OFFERS_SORTING_OPTION,
   isAuth: true
 };
 
@@ -17,6 +18,21 @@ const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(changeCityAction, (state, action) => {
       state.currentCityName = action.payload;
+    })
+    .addCase(sortByPopularAction, (state) => {
+      state.offersByCurrentCity = state.offersByCurrentCity.sort((a, b) => a.id - b.id);
+    })
+    .addCase(sortByPriceLowToHighAction, (state) => {
+      state.offersByCurrentCity = state.offersByCurrentCity.sort((a, b) => a.price - b.price);
+    })
+    .addCase(sortByPriceHighToLowAction, (state) => {
+      state.offersByCurrentCity = state.offersByCurrentCity.sort((a, b) => b.price - a.price);
+    })
+    .addCase(sortByRatedFirstAction, (state) => {
+      state.offersByCurrentCity = state.offersByCurrentCity.sort((a, b) => b.rating - a.rating);
+    })
+    .addCase(changeSortingTypeAction, (state, action) => {
+      state.sortingType = action.payload;
     });
 });
 
