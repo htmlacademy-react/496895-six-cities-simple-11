11e8136190a4f680a0e015567b4cf42ad519ea79
TroshinSerg <serge-troshin@yandex.ru>
2022-11-21 15:06:15 +0300

    Добавляет действия для сортировки

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,9 +1,21 @@
 import {createAction} from '@reduxjs/toolkit';
+import {TSortingEnum} from '../types/types';
 
 const getOffersAction = createAction('offers/getOffersAction');
 const changeCityAction = createAction('offers/changeCityAction', (value: string) => ({payload: value}));
 
+const sortByPopularAction = createAction('offers/sortByPopularAction');
+const sortByPriceLowToHighAction = createAction('offers/sortByPriceLowToHighAction');
+const sortByPriceHighToLowAction = createAction('offers/sortByPriceHighToLowAction');
+const sortByRatedFirstAction = createAction('offers/sortByRatedFirstAction');
+const changeSortingTypeAction = createAction('offers/changeSortingTypeAction', (value: TSortingEnum) => ({payload: value}));
+
 export {
   getOffersAction,
-  changeCityAction
+  changeCityAction,
+  sortByPopularAction,
+  sortByPriceLowToHighAction,
+  sortByPriceHighToLowAction,
+  sortByRatedFirstAction,
+  changeSortingTypeAction
 };
