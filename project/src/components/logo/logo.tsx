2d45a602fda1da022c36f19788448416080797ee
TroshinSerg <serge-troshin@yandex.ru>
2022-11-30 18:38:42 +0300

    Изменяет имена пропсов

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -2,16 +2,16 @@ import {Link} from 'react-router-dom';
 import {AppRoute} from '../../constants/constants';
 
 type TLogoProps = {
-  isMainPage: boolean;
+  isMainScreen: boolean;
 }
-function Logo({isMainPage}: TLogoProps): JSX.Element {
+function Logo({isMainScreen}: TLogoProps): JSX.Element {
   return (
     <>
-      {isMainPage &&
+      {isMainScreen &&
       <a className="header__logo-link header__logo-link--active">
         <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
       </a>}
-      {!isMainPage &&
+      {!isMainScreen &&
       <Link className="header__logo-link header__logo-link--active" to={AppRoute.Root}>
         <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
       </Link>}
