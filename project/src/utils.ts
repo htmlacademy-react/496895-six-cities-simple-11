9e755c1586927be9aed10975408070f6addfb5cd
TroshinSerg <serge-troshin@yandex.ru>
2022-12-12 19:50:49 +0300

    Выносит функцию фильтрации предложений по городу в модуль utils

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -1,7 +1,10 @@
 import {AuthorizationStatus} from './constants/constants';
+import { TOffer } from './types/types';
 
 const isAuthorized = (status: string) => status === AuthorizationStatus.Auth;
+const filterOffers = (offers: TOffer[], cityName: string) => offers.filter((offer) => offer.city.name === cityName);
 
 export {
-  isAuthorized
+  isAuthorized,
+  filterOffers
 };
